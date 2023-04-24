14c62af1ec23dd750a83c214b9f12296aa6b030e
napurnama <purnama.naufal.adli@gmail.com>
2023-04-24 07:21:07 +0700

    establish connection to db using config

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/PBKKE08/FP-BE/echo-rest/db"
 	"github.com/PBKKE08/FP-BE/echo-rest/routes"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	db.Init()
 	e := routes.Init()
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.GET("/", func(c echo.Context) error{
+		return c.String(http.StatusOK, "Hello from echo!")
+	})
+
+	e.Logger.Fatal(e.Start(":1234"))
 }
